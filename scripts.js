7766cd4f77304737c9f2569a26e90456c25af002
jabattack1 <peter.hahn@live.com>
2019-03-16 12:22:27 -0400

    redesign page

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 console.log('yo');
+
 // $(window).load(function(){
 //     setTimeout(function() {
 //       $('.loader').fadeOut();
 //     }, 2000);
 // });
 
-var movie1= "<iframe width='1000' height='700' src='https://www.youtube.com/embed/EKeTpxznWe8?autoplay=1&&mute=1&controls=0&loop=1&showinfo=0&playlist=EKeTpxznWe8' frameborder='0' scrolling='no' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+var movie1= "<iframe width='800' height='600' src='https://www.youtube.com/embed/EKeTpxznWe8?autoplay=1&&mute=1&controls=0&loop=1&showinfo=0&playlist=EKeTpxznWe8' frameborder='0' scrolling='no' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+
+
 // var movie2= "<img id='movieb' src='https://i.imgur.com/J6sjDIU.gif' />"
 // var movie3= "<img id='moviec' src='https://i.imgur.com/Yy5iWrE.gif' />"
 // var movie4= "<img id='movied' src='https://i.imgur.com/Ix3pFXW.gif' />"
